e2da67ac61a4aad8993530d12cda095d9e2bb909
vikram(wsl) <vikramjit360@gmail.com>
2025-11-07 12:56:31 +0530

    feat: set up Prime React

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
-import { useState } from 'react'
+import { useEffect, useState } from "react";
+
+const API_BASE_URL = "https://api.artic.edu/api/v1/artworks";
 
 function App() {
+  const [page, setPage] = useState<number>(1);
+  const [data, setData] = useState([]);
 
-  return (
-    <>
-      Hello World
-    </>
-  )
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const rawData = await fetch(`${API_BASE_URL}?page${page}`);
+        const jsonData = await rawData.json();
+        setData(jsonData);
+        console.log(jsonData);
+      } catch (error) {
+        console.log("Could not fetch the artwork data");
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, [page]);
+
+  return <>Hello World</>;
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { PrimeReactProvider } from 'primereact/api';
+
+// import 'primeicons/primeicons.css';
+// import 'primeflex/primeflex.css';
+import 'primereact/resources/primereact.css';
+import 'primereact/resources/themes/lara-light-indigo/theme.css';
+
 import './index.css'
 import App from './App.tsx'
 
